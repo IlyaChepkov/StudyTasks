a6b1a2019f35c87dc3d8771bdfe7240860b69394
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2022-10-27 20:42:17 +0300

    CaseInit

---
+++ IlyaCode/Case.cs
@@ -0,0 +1,17 @@
+﻿namespace IlyaCode
+{
+    internal static class Case
+    {
+        public static void Do()
+        {
+            Console.WriteLine("Введите номер задачи");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Case1");
+                    break;
+            }
+        }
+    }
+}
