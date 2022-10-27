d2e011d6dea661de27355849c179ec7310059d51
IlyaChepkov <ilya.chepkov2011@gmail.com>
2022-10-27 21:02:07 +0300

    Сделал case1

--- IlyaCode/Case.cs
+++ IlyaCode/Case.cs
@@ -10,6 +10,45 @@
                 case 1:
                     {
                         Console.WriteLine("Case1");
+                        switch (Convert.ToByte(Console.ReadLine()))
+                        {
+                            case 1:
+                                {
+                                    Console.WriteLine("Понедельник");
+                                }
+                                break;
+                            case 2:
+                                {
+                                    Console.WriteLine("Вторник");
+                                }
+                                break;
+                            case 3:
+                                {
+                                    Console.WriteLine("Среда");
+                                }
+                                break;
+                            case 4:
+                                {
+                                    Console.WriteLine("Четверг");
+                                }
+                                break;
+                            case 5:
+                                {
+                                    Console.WriteLine("Пятница");
+                                }
+                                break;
+                            case 6:
+                                {
+                                    Console.WriteLine("Суббота");
+                                }
+                                break;
+                            case 7:
+                                {
+                                    Console.WriteLine("Воскресенье");
+                                }
+                                break;
+                        }
+                    }
                     break;
             }
         }
