567fa9c8f6216e35f55c77c60c76f3294baf1634
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2022-11-21 00:43:51 +0300

    For1-2 и заготовка для 3-4

--- IlyaCode/For.cs
+++ IlyaCode/For.cs
@@ -10,6 +10,48 @@
                 case 1:
                     {
                         Console.WriteLine("For1");
+                        int k = Convert.ToInt32(Console.ReadLine());
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < k; i++)
+                        {
+                            Console.WriteLine(n);
+                        }
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine("For2");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        for (int i = a; i <= b; i++)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        Console.WriteLine(b - a + 1);
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.WriteLine("For3");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        for (int i = a; i <= b; i++)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        Console.WriteLine(b - a + 1);
+                    }
+                    break;
+                case 4:
+                    {
+                        Console.WriteLine("For4");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        for (int i = a; i <= b; i++)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        Console.WriteLine(b - a + 1);
                     }
                     break;
             }
