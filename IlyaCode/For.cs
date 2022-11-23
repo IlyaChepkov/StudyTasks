e34857c3bb3b98160db170c527cb74a4f2789ec2
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2022-11-23 09:28:30 +0300

    For 3-7

--- IlyaCode/For.cs
+++ IlyaCode/For.cs
@@ -35,23 +35,64 @@
                         Console.WriteLine("For3");
                         int a = Convert.ToInt32(Console.ReadLine());
                         int b = Convert.ToInt32(Console.ReadLine());
-                        for (int i = a; i <= b; i++)
+                        for (int i = b - 1; i > a; i--)
                         {
                             Console.WriteLine(i);
                         }
-                        Console.WriteLine(b - a + 1);
+                        Console.WriteLine(b - a - 1);
                     }
                     break;
                 case 4:
                     {
                         Console.WriteLine("For4");
-                        int a = Convert.ToInt32(Console.ReadLine());
-                        int b = Convert.ToInt32(Console.ReadLine());
-                        for (int i = a; i <= b; i++)
+                        double a = Convert.ToDouble(Console.ReadLine());
+                        for (double i = a; i <= a * 10; i += a)
                         {
                             Console.WriteLine(i);
                         }
-                        Console.WriteLine(b - a + 1);
+                    }
+                    break;
+                case 5:
+                    {
+                        Console.WriteLine("For5");
+                        double a = Convert.ToDouble(Console.ReadLine()) / 10;
+                        for (double i = a; i <= a * 10; i += a)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    break;
+                case 6:
+                    {
+                        Console.WriteLine("For6");
+                        double a = Convert.ToDouble(Console.ReadLine()) / 5;
+                        for (double i = a * 6; i <= a * 10; i += a)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    break;
+                case 7:
+                    {
+                        Console.WriteLine("For7");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        int sum = 0;
+                        for (int i = b; i >= a; i--)
+                        {
+                            sum += i;
+                        }
+                        Console.WriteLine(sum);
+                    }
+                    break;
+                case 8:
+                    {
+                        Console.WriteLine("For4");
+                        double a = Convert.ToDouble(Console.ReadLine());
+                        for (double i = a; i <= a * 10; i += a)
+                        {
+                            Console.WriteLine(i);
+                        }
                     }
                     break;
             }
