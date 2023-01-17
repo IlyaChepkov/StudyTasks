dae4383b25c80370ddf4b6bcc2e82adc71d098ba
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2023-01-17 21:46:40 +0300

    Array6-15

--- IlyaCode/Array.cs
+++ IlyaCode/Array.cs
@@ -92,5 +92,170 @@
                 Console.WriteLine(array[i]);
             }
         }
+
+        public static void Array6()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            array[0] = Convert.ToInt32(Console.ReadLine());
+            array[1] = Convert.ToInt32(Console.ReadLine());
+            array[2] = array[0] + array[1];
+            for (int i = 3; i < n; i++)
+            {
+                array[i] = array[i - 1] * 2;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public static void Array7()
+        {
+            string[] array = new string[Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public static void Array8()
+        {
+            int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int k = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    Console.WriteLine(array[i]);
+                    k++;
+                }
+            }
+            Console.WriteLine(k);
+        }
+
+        public static void Array9()
+        {
+            int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int k = 0;
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    Console.WriteLine(array[i]);
+                    k++;
+                }
+            }
+            Console.WriteLine(k);
+        }
+
+        public static void Array10()
+        {
+            int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+
+        public static void Array11()
+        {
+            int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+            int k = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = k - 1; i < array.Length; i += k)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public static void Array12()
+        {
+            int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 1; i < array.Length; i += 2)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public static void Array13()
+        {
+            int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = array.Length - 1; i >= 0; i -= 2)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public static void Array14()
+        {
+            int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 1; i < array.Length; i += 2)
+            {
+                Console.WriteLine(array[i]);
+            }
+            for (int i = 0; i < array.Length; i += 2)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public static void Array15()
+        {
+            int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i += 2)
+            {
+                Console.WriteLine(array[i]);
+            }
+            for (int i = array.Length / 2 * 2 - 1; i >= 1; i -= 2)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
     }
 }
