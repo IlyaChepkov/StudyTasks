8bc05f3b511bdc589429d6b6eebce0d877d5888e
IlyaChepkov <ilya.chepkov2011@gmail.com>
2023-06-19 21:07:19 +0300

    recur 7 - 18

--- IlyaCode/RecurInit.cs
+++ IlyaCode/RecurInit.cs
@@ -4,6 +4,7 @@
     {
 
         static int[] fibMemory = new int[20];
+        static int[,] combinMemory = new int[21, 21];
 
         public static double FactN(int n)
         {
@@ -38,14 +39,13 @@
                 }
                 else
                 {
-                    return x * PowerN(x, n - 1); 
+                    return x * PowerN(x, n - 1);
                 }
             }
             else
             {
                 return 1 / PowerN(x, -n);
             }
-            return n * Fact2(n - 2);
         }
 
         public static int Fib1(int n, ref int count)
@@ -66,7 +66,10 @@
                 {
                     fibMemory[n - 1] = 1;
                 }
-                fibMemory[n - 1] = Fib2(n - 2) + Fib2(n - 1);
+                else
+                {
+                    fibMemory[n - 1] = Fib2(n - 2) + Fib2(n - 1);
+                }
             }
             return fibMemory[n - 1];
         }
@@ -79,6 +82,202 @@
                 return 1;
             }
             return Combin1(n - 1, k, ref count) + Combin1(n - 1, k - 1, ref count);
+        }
+
+        public static int Combin2(int n, int k)
+        {
+            if (combinMemory[n, k] == 0)
+            {
+                if (k == 0 || k == n)
+                {
+                    combinMemory[n, k] = 1;
+                }
+                else
+                {
+                    combinMemory[n, k] = Combin2(n - 1, k) + Combin2(n - 1, k - 1);
+                }
+            }
+            return combinMemory[n, k];
+        }
+
+        public static double RootK(double x, int k, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                double res = RootK(x, k, n - 1);
+                return res - (res - x / PowerN(res, k - 1)) / k;
+            }
+        }
+
+        public static uint NOD(uint a, uint b)
+        {
+            if (b == 0) return a;
+            else return NOD(b, a % b);
+        }
+
+        public static int DigitSum(int k)
+        {
+            if (k < 10) return k;
+            return k % 10 + DigitSum(k / 10);
+        }
+
+        public static int MaxElem(int[] a, int n)
+        {
+            if (n == 1)
+            {
+                return a[0];
+            }
+            else
+            {
+                int max = MaxElem(a, n - 1);
+                return max > a[n - 1] ? max : a[n - 1];
+            }
+        }
+
+        public static int DigitCount(string s)
+        {
+            if (s.Length == 1) return Char.IsDigit(s[0]) ? 1 : 0;
+            else
+            {
+                return DigitCount(s.Substring(1)) + (Char.IsDigit(s[0]) ? 1 : 0);
+            }
+        }
+
+        public static bool Palindrom(string s)
+        {
+            if (s.Length == 1)
+            {
+                return true;
+            }
+            else if (s.Length == 2)
+            {
+                return s[0] == s[1];
+            }
+            else
+            {
+                return s[0] == s[^1] && Palindrom(s.Substring(1, s.Length - 2));
+            }
+        }
+
+        public static int Expretion1(string s)
+        {
+            if (s.Length == 1) return byte.Parse(s);
+            else
+            {
+                if (s[^2] == '+')
+                    return Expretion1(s.Remove(s.Length - 2)) + byte.Parse(s[^1].ToString());
+                else
+                    return Expretion1(s.Remove(s.Length - 2)) - byte.Parse(s[^1].ToString());
+            }
+        }
+
+        public static int Expretion2(string s)
+        {
+            int plus = s.LastIndexOf('+');
+            int minus = s.LastIndexOf('-');
+            if (plus == -1 && minus == -1) return Term2(s);
+            else
+            {
+                if (plus > minus)
+                    return Expretion2(s.Remove(plus)) + Term2(s.Substring(plus + 1));
+                else
+                    return Expretion2(s.Remove(minus)) - Term2(s.Substring(minus + 1));
+            }
+        }
+
+        public static int Term2(string s)
+        {
+            if (s.Length == 1)
+            {
+                return byte.Parse(s);
+            }
+            else return Term2(s.Remove(s.Length - 2)) * byte.Parse(s[^1].ToString());
+        }
+
+        public static int Expretion3(string s)
+        {
+            int plus = s.LastIndexOf('+');
+            int minus = s.LastIndexOf('-');
+            if (plus == -1 && minus == -1) return Term3(s);
+            else
+            {
+                if (plus > minus)
+                    return Expretion3(s.Remove(plus)) + Term3(s.Substring(plus + 1));
+                else
+                    return Expretion3(s.Remove(minus)) - Term3(s.Substring(minus + 1));
+            }
+        }
+
+        public static int Term3(string s)
+        {
+            int mul = s.LastIndexOf('*');
+            if (mul == -1)
+            {
+                return Elem3(s);
+            }
+            else return Term3(s.Remove(mul)) * Elem3(s.Substring(mul + 1));
+        }
+
+        public static int Elem3(string s)
+        {
+            if (s.Length == 1) return byte.Parse(s);
+            else return Expretion3(s.Substring(1, s.Length - 2));
+        }
+
+        public static int Expretion4(string s, ref int index) //Самая_сложная_задача_на_свете100%!!!(string s)
+        {
+            int first;
+            int second;
+            char symbol;
+            if (s[index] == '(')
+            {
+                index++;
+                first = Expretion4(s, ref index);
+                symbol = s[index];
+                index++;
+                second = Expretion4(s, ref index);
+                index++;
+                return symbol switch
+                {
+                    '+' => first + second,
+                    '-' => first - second,
+                    '*' => first * second
+                };
+            }
+            else
+            {
+                first = byte.Parse(s[index].ToString());
+                index++;
+                return first;
+            }
+        }
+
+        public static bool CheckExpretion4(string s, ref int index) //Самая_сложная_задача_на_свете100%!!!(string s)
+        {
+            if (s[index] == '(')
+            {
+                index++;
+                if (!CheckExpretion4(s, ref index)) return false;
+                if (!(s[index] == '+' || s[index] == '-' || s[index] == '*')) return false;
+                index++;
+                if (!CheckExpretion4(s, ref index)) return false;
+                if (s[index] != ')') return false;
+                index++;
+                return true;
+            }
+            else
+            {
+                if (Char.IsDigit(s[index]))
+                {
+                    index++;
+                    return true;
+                }
+                return false;
+            }
         }
     }
 }
--- IlyaCode/RecurUse.cs
+++ IlyaCode/RecurUse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IlyaCode
+﻿namespace IlyaCode
 {
     internal partial class Recur
     {
@@ -72,6 +66,95 @@ namespace IlyaCode
                 Console.WriteLine(Combin1(x, int.Parse(Console.ReadLine()), ref count));
                 Console.WriteLine(count);
             }
+        }
+
+        public static void Recur7()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 5; i++)
+            {
+                int k = int.Parse(Console.ReadLine());
+                Console.WriteLine(Combin2(n, k));
+            }
+        }
+
+        public static void Recur8()
+        {
+            double x = double.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 5; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(RootK(x, k, n));
+            }
+        }
+
+        public static void Recur9()
+        {
+            uint a = uint.Parse(Console.ReadLine());
+            for (int i = 0; i < 3; i++)
+            {
+                uint bcd = uint.Parse(Console.ReadLine());
+                Console.WriteLine(NOD(a, bcd));
+            }
+        }
+
+        public static void Recur10()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int bcd = int.Parse(Console.ReadLine());
+                Console.WriteLine(DigitSum(bcd));
+            }
+        }
+
+        public static void Recur11()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int[] bcd = Array.IntInput(n);
+                Console.WriteLine(MaxElem(bcd, n));
+            }
+        }
+
+        public static void Recur12()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                string s = Console.ReadLine();
+                Console.WriteLine(DigitCount(s));
+            }
+        }
+
+        public static void Recur13()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                string s = Console.ReadLine();
+                Console.WriteLine(Palindrom(s));
+            }
+        }
+
+        public static void Recur14() =>
+            Console.WriteLine(Expretion1(Console.ReadLine()));
+
+        public static void Recur15() =>
+            Console.WriteLine(Expretion2(Console.ReadLine()));
+
+        public static void Recur16() =>
+            Console.WriteLine(Expretion3(Console.ReadLine()));
+
+        public static void Recur17()
+        {
+            int a = 0;
+            Console.WriteLine(Expretion4(Console.ReadLine(), ref a));
+        }
+
+        public static void Recur18()
+        {
+            int a = 0;
+            Console.WriteLine(CheckExpretion4(Console.ReadLine(), ref a));
         }
     }
 }
